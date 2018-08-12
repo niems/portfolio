c886abbaa2437100e6806651fef0895da67d28f2
Zach Niemann <czniemann1990@gmail.com>
2018-08-12 10:46:03 -0500

    navbar scrolling from links fixed. Currently working on mouse wheel scrolling

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,15 +6,35 @@ import Projects from './projects';
 import Contact from './contact';
 import './style/layout.css';
 
+function scrollIntoView(delta, scrollIndex, display) {
+    if ( delta === 0) { 
+        console.log('onWheel() registered a deltaY value of zero...');
+        return undefined; 
+    }
+
+    else if ( delta > 0 ) { //scroll down
+        scrollIndex++; //scrolls to the next page
+
+        if ( scrollIndex > (display.length  - 1) ) {
+            scrollIndex = 0; //reset values - starts at home page
+        }
+        
+    }   
+    
+    else { //scroll up
+        scrollIndex--; //scrolls to the previous page
+        
+        if ( scrollIndex < 0 ) {
+            scrollIndex = display.length - 1;
+        } 
+    }
+
+    return scrollIndex;
+}
+
 class Layout extends Component {
     constructor(props) {
         super(props);
-
-        this.scrollOptions = {
-            block: 'start',
-            inline: 'nearest',
-            behavior: 'smooth'
-        };
         
         this.homeRef = undefined;
         this.experienceRef = undefined;
@@ -23,28 +43,71 @@ class Layout extends Component {
         
         this.navbarSelect = this.navbarSelect.bind(this); //selected a navbar section
         this.setRef = this.setRef.bind(this); 
+
+        this.onWheel = this.onWheel.bind(this);
+        this.scrollIntoView = this.scrollIntoView.bind(this);
+        this.scrollOptions = {
+            block: 'start',
+            inline: 'nearest',
+            behavior: 'smooth'
+        };
+
+        this.scrollIndex = 0; //index of the current page displayed
+        this.display = [
+            'home',
+            'experience',
+            'projects',
+            'contact'
+        ];
+    }
+
+    onWheel(e) {
+        console.log('onWheel():');
+        console.log(`\t delta mode: ${e.deltaMode}`);
+        console.log(`\t delta x: ${e.deltaX}`);
+        console.log(`\t delta y: ${e.deltaY}`);        
+        console.log(`\t delta z: ${e.deltaZ}`);
+
+        //console.log( `page index: ${this.display[this.currentPage.name]['index']}` );
+        this.scrollIndex = scrollIntoView( e.deltaY, this.scrollIndex, this.display ); //returns index of new page to scroll to
+
+        this.scrollIntoView( this.display[this.scrollIndex] ); 
+    }
+
+    scrollIntoView(viewName) {
+        console.log(`scrollIntoView(): ${viewName}`);
+
+        switch(viewName) {
+            case 'navbar-home-section':
+            case 'home':
+                this.homeRef.scrollIntoView( this.scrollOptions );
+                break;
+            
+            case 'navbar-experience-section':
+            case 'experience':
+                this.experienceRef.scrollIntoView( this.scrollOptions );
+                break;
+
+            case 'navbar-projects-section':
+            case 'projects':
+                this.projectsRef.scrollIntoView( this.scrollOptions );
+                break;
+            
+            case 'navbar-contact-section':
+            case 'contact':
+                this.contactRef.scrollIntoView( this.scrollOptions );
+                break;
+            
+            default:
+                console.log('scrollIntoView() case not defined');
+                break;
+        }
     }
 
     navbarSelect(e) {
         e.preventDefault();
 
-        switch(e.currentTarget.id) {
-            case 'navbar-home-section':
-                this.homeRef.scrollIntoView( this.scrollOptions );
-                break;
-
-            case 'navbar-experience-section':
-                this.experienceRef.scrollIntoView( this.scrollOptions );
-                break;
-
-            case 'navbar-projects-section':
-                this.projectsRef.scrollIntoView( this.scrollOptions );
-                break;
-            
-            case 'navbar-contact-section':
-                this.contactRef.scrollIntoView( this.scrollOptions );
-                break;
-        }
+       this.scrollIntoView( e.currentTarget.id ); 
     }
 
     setRef(type, element) {
@@ -64,12 +127,16 @@ class Layout extends Component {
             case 'contact':
                 this.contactRef = element;
                 break;
+            
+            default:
+            console.log('scrollIntoView() case not defined');
+            break;
         }
     }
 
     render() {
         return (
-            <div id='layout-container'>
+            <div id='layout-container' onScroll={this.onScroll} onWheel={this.onWheel}>
                 <Navbar selection={this.navbarSelect} />
                 <svg>
                     <circle />
