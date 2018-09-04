0bbf2c9337435ed5740365c085c32f95f1d0b7c1
Zach Niemann <czniemann1990@gmail.com>
2018-09-04 14:28:44 -0500

    navbar section selection now based on the scroll height instead of which section the user clicked. Saving before removing state from navbar since it is no longer needed.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,6 +18,8 @@ class Navbar extends Component {
         };
 
         this.updateSelection = this.updateSelection.bind(this);
+
+        //remove these
         this.removeActiveStatus = this.removeActiveStatus.bind(this);
         this.addActiveStatus = this.addActiveStatus.bind(this);
     }
@@ -43,6 +45,7 @@ class Navbar extends Component {
     }
 
     removeActiveStatus() {
+        /*
         switch(this.state.displayed) {
             case 'home':
                 this.homeRef.classList.remove('selected');
@@ -64,9 +67,11 @@ class Navbar extends Component {
                 this.menuRef.classList.remove('selected');
                 break;
         }
+        */
     }
 
     addActiveStatus(id) {
+        /*
         switch(id) {
             case 'home':
                 this.homeRef.classList.add('selected');
@@ -87,18 +92,24 @@ class Navbar extends Component {
             case 'menu':
                 this.menuRef.classList.add('selected');
                 break;
-        }
+            }
+       */
     }
 
     render() {
+        const homeClasses = this.props.section === 'home' ? 'icon-text desktop-only selected' : 'icon-text desktop-only';
+        const portfolioClasses = this.props.section === 'portfolio' ? 'icon-text desktop-only selected' : 'icon-text desktop-only';
+        const experimentsClasses = this.props.section === 'experiments' ? 'icon-text desktop-only selected' : 'icon-text desktop-only';
+        const contactClasses = this.props.section === 'contact' ? 'icon-text desktop-only selected' : 'icon-text desktop-only';
+
         return (
             <ThemeContext.Consumer>
                 {({ theme }) => (
                     <nav className={`navbar-container ${theme.name}`} >
-                        <div className='icon-text desktop-only selected' id='home' onClick={this.updateSelection} ref={element => {this.homeRef = element;} }>Home</div>
-                        <div className='icon-text desktop-only' id='portfolio' onClick={this.updateSelection} ref={element => {this.portfolioRef = element;} }>Portfolio</div>
-                        <div className='icon-text desktop-only' id='experiments' onClick={this.updateSelection} ref={element => {this.experimentsRef = element;} }>Experiments</div>
-                        <div className='icon-text desktop-only' id='contact' onClick={this.updateSelection} ref={element => {this.contactRef = element;} }>Contact</div>                        
+                        <div className={homeClasses} id='home' onClick={this.updateSelection} ref={element => {this.homeRef = element;} }>Home</div>
+                        <div className={portfolioClasses} id='portfolio' onClick={this.updateSelection} ref={element => {this.portfolioRef = element;} }>Portfolio</div>
+                        <div className={experimentsClasses} id='experiments' onClick={this.updateSelection} ref={element => {this.experimentsRef = element;} }>Experiments</div>
+                        <div className={contactClasses} id='contact' onClick={this.updateSelection} ref={element => {this.contactRef = element;} }>Contact</div>                        
                         
                         <div className='icon-text mobile-only' id='menu' onClick={this.props.onMenu} ref={element => {this.menuRef = element;} }>
                             <img className={`navbar-icon ${theme.name}`} id='menu-img' src='./images/menu.svg' alt='failed to load menu icon D:' />
--- src/components/view.js
+++ src/components/view.js
@@ -15,11 +15,7 @@ class View extends Component {
         super(props);
         
         this.state = {
-            displayed: {
-                name: 'home',
-                component: <HomePage setRef={el => this.homeRef = el} />
-            },
-
+            currentSection: 'home', //section on the majority of the screen
             displayMenu: null
         };
 
@@ -30,6 +26,25 @@ class View extends Component {
             'contact'
         ];
 
+        this.displayInfo = {
+            home: {
+                startHeight: -1,
+                endHeight: -1
+            },
+            portfolio: {
+                startHeight: -1,
+                endHeight: -1
+            },
+            experiments: {
+                startHeight: -1,
+                endHeight: -1
+            },
+            contact: {
+                startHeight: -1,
+                endHeight: -1
+            },
+        };
+
         this.homeRef = null;
         this.portfolioRef = null;
         this.experimentsRef = null;
@@ -38,12 +53,19 @@ class View extends Component {
         this.onMenuToggle = this.onMenuToggle.bind(this); //toggles the menu on/off
         this.updateDisplayedPage = this.updateDisplayedPage.bind(this); 
 
+        this.getSectionHeights = this.getSectionHeights.bind(this); //gets the start/end scroll height for each section
+
         //TESTING ONLY
         this.testing = false;
         this.testLoop = this.testLoop.bind(this);
+
+        this.viewRef = null;
+        this.onScroll = this.onScroll.bind(this);
     }
 
     componentDidMount() {
+        this.getSectionHeights();
+
         if ( this.testing )
             this.testLoop();
     }
@@ -119,13 +141,64 @@ class View extends Component {
         }
     }   
 
+    onScroll(e) {
+        e.preventDefault();
+        
+        const csh = this.viewRef.scrollTop; //csh = current scroll height
+        let currentSection = '';
+
+        if ( csh >= this.displayInfo.contact.startHeight ) {
+            currentSection = 'contact';
+        }
+
+        else if ( csh >= this.displayInfo.experiments.startHeight ) {
+            currentSection = 'experiments';
+        }
+
+        else if ( csh >= this.displayInfo.portfolio.startHeight ) {
+            currentSection = 'portfolio';
+        }
+
+        else {
+            currentSection = 'home';
+        }
+
+        console.log(`current section: ${currentSection}\n`);
+
+        if ( currentSection !== this.state.currentSection ) {
+            this.setState({ currentSection });
+        }
+    }
+
+    getSectionHeights() {
+        console.log(`section: ${this.displayInfo['home'].startHeight}`);
+        
+        let currentHeight = this.homeRef.scrollHeight;
+        this.displayInfo.home.startHeight = 0;
+        this.displayInfo.home.endHeight = currentHeight;
+
+        currentHeight += this.portfolioRef.scrollHeight;
+        this.displayInfo.portfolio.startHeight = this.displayInfo.home.endHeight;
+        this.displayInfo.portfolio.endHeight = currentHeight;
+
+        currentHeight += this.experimentsRef.scrollHeight;
+        this.displayInfo.experiments.startHeight = this.displayInfo.portfolio.endHeight;
+        this.displayInfo.experiments.endHeight = currentHeight;
+
+        
+
+        currentHeight += this.contactRef.scrollHeight;
+        this.displayInfo.contact.startHeight = this.displayInfo.experiments.endHeight;
+        this.displayInfo.contact.endHeight = currentHeight;
+    }
+
     render() {
         return (
-            <div id='view-container'>
-                <Navbar selection={this.updateDisplayedPage} onMenu={this.onMenuToggle} />
+            <div id='view-container' onScroll={this.onScroll}>
+                <Navbar section={this.state.currentSection} selection={this.updateDisplayedPage} onMenu={this.onMenuToggle} />
                 {this.state.displayMenu}
 
-                <div id='sections-container'>                    
+                <div id='sections-container' ref={el => this.viewRef = el}>                    
                     <HomePage setRef={el => this.homeRef = el} selection={this.updateDisplayedPage} />
                     <Portfolio setRef={el => this.portfolioRef = el} />
                     <Experiments setRef={el => this.experimentsRef = el} />
