8a5e97d2403166ff004130ccfb8d3528e51d8da1
Zach Niemann <czniemann1990@gmail.com>
2018-09-05 16:28:03 -0500

    added conditionally rendered content for each project onHover

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import AutoText from './autoText';
 import './style/portfolio.css';
 
+/*
+<img className='portfolio-project-img' src='./images/projects/placeholder-project.png' alt='' />
+
+*/
 function DisplayProject(props) {
     return (
         <div className='portfolio-project-container'>
 
+            <div className='portfolio-project-hover-info'>
+                <h3 className='portfolio-project-name'>Project name 1</h3>
+                <h4 className='portfolio-project-frameworks'>Electron + Node + React</h4>
+                <button className='portfolio-project-btn'>learn more</button>
+            </div>
         </div>
     );
 }
