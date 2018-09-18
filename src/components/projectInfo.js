e3eadaf1253becad48d5f682396759a55c9c57cd
Zach Niemann <czniemann1990@gmail.com>
2018-09-18 16:37:05 -0500

    updated display image for each project. Also added gif as the first image for each

--- src/components/projectInfo.js
+++ src/components/projectInfo.js
@@ -4,6 +4,7 @@ const projects = {
         type: 'Web chat app',
         description: 'Roulette provides anonymous video/audio chatting using WebRTC, with Node.js and Socket.IO on the back-end. The learning curve for this freelance project was up quite a hill: the client wanted a video/audio chatting site, so I set out learning the WebRTC API - no walk in the park...',
         images: [
+            'https://media.giphy.com/media/pOlr6w3EMOQkeDHhbF/giphy.gif',
             './images/projects/roulette/landing.png',
             './images/projects/roulette/waiting-area.png',
             './images/projects/roulette/chat-joined.png',
@@ -16,6 +17,7 @@ const projects = {
         type: 'Desktop/Web chat app',
         description: 'Whisper is the first chat app I coded using React! While private messaging and group chat work on the web, I also coded it as a desktop application using Electron (wraps the web app for distribution on Windows/Mac/Linux).',
         images: [
+            'https://media.giphy.com/media/5wFGesz1P7H9aTTbBw/giphy.gif',
             './images/projects/whisper/chatting.png',
             './images/projects/whisper/landing.png',
             './images/projects/whisper/sign-in.png',
@@ -27,6 +29,7 @@ const projects = {
         type: 'Desktop/Web dictionary search app',
         description: 'Word connect placeholder text...',
         images: [
+            'https://media.giphy.com/media/9AINN6phxeZyg6RIZf/giphy.gif',
             './images/projects/word-connect/display.png',
             './images/projects/word-connect/loading.png',
         ]
