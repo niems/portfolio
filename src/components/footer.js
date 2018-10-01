c1b0a22259cc3e828f4c07d28ee7ca75375d7e6b
Zach Niemann <czniemann1990@gmail.com>
2018-10-01 16:09:36 -0500

    moved contact form to footer. Moved social links to footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import './contactForm';
 import './style/footer.css';
+import ContactForm from './contactForm';
+
+function DisplaySocialLinks(props) {
+    return (
+        <div id='contact-social-links'>
+            <a href='https://github.com/niems' id='github-link' className='btn slide-in smush-mid contact-link' target='_blank' rel='noopener noreferrer'>
+                <img src='./images/social/github.svg' alt='Github profile' />
+            </a>
+
+            <a href='https://dribbble.com/ni3ms' id='dribbble-link' className='btn slide-in smush-mid contact-link' target='_blank' rel='noopener noreferrer'>
+                <img src='./images/social/dribbble-icon.svg' alt='Dribbble profile' />
+            </a>
+
+            <a href='https://codepen.io/niems/full/MBBWoK/' id='codepen-link' className='btn slide-in smush-mid contact-link' target='_blank' rel='noopener noreferrer'>
+                <img src='./images/social/codepen.svg' alt='Codepen profile' />
+            </a>
+        </div>
+    );
+}
 
 const Footer = (props) => {
     return (
         <footer id='footer-container'>
-            <span id='footer-description'>Designed & Coded by Zach Niemann.</span>
+            <ContactForm />
+            <DisplaySocialLinks />
+            <div id='footer-description'>Designed / Coded using React & Sass by Zach Niemann</div>
         </footer>
     );
 };
