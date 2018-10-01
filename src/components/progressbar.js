0714fbe3b17a41e69feb24e82e8e630ef058c7f9
Zach Niemann <czniemann1990@gmail.com>
2018-10-01 18:54:28 -0500

    updated progressbar design - currently only done for mobile-first

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -1,87 +1,84 @@
 import React from 'react';
 import './style/progressbar.css';
 
-function ProgressBarIcons({ name, img, skillLevel, link }) {
+function ProgressBarIcon({ name, img, link }) {
     return (
         <a className='progress-bar-wrapper' href={link} target='_blank' alt={`Link to ${name} for more info`}>
-            <div className='progress-text-bar-container'>
-                <span className='progress-bar-img-container'>
-                    <img className='progress-bar-img' src={img} alt={`${name} logo`} />
-                </span>
-                
-                <div className='progress-bar-container'>
-                    <span className='progress-bar' style={{width: skillLevel}}>
-                        <span className='progress-bar-skill-level'>{skillLevel}</span>
-                    </span>
-                </div> 
-            </div>
+            <span className='progress-bar-img-container'>
+                <img className='progress-bar-img' src={img} alt={`${name} logo`} />
+            </span>
 
             <span className='progress-bar-text'>{name}</span>
         </a>
     );
 }
 
+function ProgressBarsSection({ title, skills }) {
+    const displaySkills = skills.map(skill => (
+        <ProgressBarIcon key={skill.name} name={skill.name} img={skill.imgPath} link={skill.link} />
+    ));
+
+    return (
+        <div className='progressbar-section'>
+            <div className='progressbar-section-description'>{title}</div>
+            {displaySkills}
+        </div>
+    );
+}
+
 export default function Progressbar(props) {
     const path = './images/icons/';
 
-    const skillList = [
+    const dailySkills = [
         {
             name: 'React',
             imgPath: `${path}react.svg`,
-            skill: '80%', 
             link: 'https://reactjs.org/',
         },
         {
             name: 'JavaScript',
             imgPath: `${path}javascript.svg`,
-            skill: '80%',
             link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-        },
-        {
-            name: 'Node.js',
-            imgPath: `${path}nodejs.svg`,
-            skill: '75%',
-            link: 'https://nodejs.org/en/about/',
-        },
-        {
-            name: 'Socket.IO',
-            imgPath: `${path}socket.io.svg`,
-            skill: '75%',
-            link: 'https://socket.io/',
-        },
-        {
-            name: 'WebRTC',
-            imgPath: `${path}webrtc.svg`,
-            skill: '70%',
-            link: 'https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API',
         },
         {
             name: 'Sass',
             imgPath: `${path}sass.svg`,
-            skill: '85%',
             link: 'https://sass-lang.com/',
         },
         {
             name: 'Git',
             imgPath: `${path}git-icon.svg`,
-            skill: '85%',
             link: 'https://git-scm.com/',
         },
         {
             name: 'HTML5',
             imgPath: `${path}html-5.svg`,
-            skill: '90%',
             link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
         },
     ];
 
-    const displayedSkills = skillList.map(skill => (
-        <ProgressBarIcons key={skill.name} name={skill.name} img={skill.imgPath} skillLevel={skill.skill} link={skill.link} />
-    ));
+    const familiarSkills = [
+        {
+            name: 'Node.js',
+            imgPath: `${path}nodejs.svg`,
+            link: 'https://nodejs.org/en/about/',
+        },
+        {
+            name: 'Socket.IO',
+            imgPath: `${path}socket.io.svg`,
+            link: 'https://socket.io/',
+        },
+        {
+            name: 'WebRTC',
+            imgPath: `${path}webrtc.svg`,
+            link: 'https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API',
+        },
+    ];
 
     return (
         <div className='all-progress-bars-container'>
-            {displayedSkills}
+            <ProgressBarsSection title='Day-to-day:' skills={dailySkills} />
+            <ProgressBarsSection title='Familiar with:' skills={familiarSkills} />
         </div>
     );
 }
