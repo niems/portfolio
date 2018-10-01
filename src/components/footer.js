bcd6da49aa898c0159f8b98c2584e2d0f65ac1d5
Zach Niemann <czniemann1990@gmail.com>
2018-10-01 15:18:34 -0500

    created initial footer component - adding to view

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/footer.css';
+
+const Footer = (props) => {
+    return (
+        <footer id='footer-container'>
+            <span id='footer-description'>Designed & Coded by Zach Niemann.</span>
+        </footer>
+    );
+};
+
+export default Footer;
