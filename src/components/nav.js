fcf8ddbf28654f0951387fe00742ede59f4a31b9
Zach Niemann <czniemann1990@gmail.com>
2018-10-02 13:17:20 -0500

    added navbar icons back. Need to link them to pages

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ class Nav extends Component {
     
     render() {
         return (
-            <nav id='gooey-menu-container'>
+            <nav id='gooey-menu-container' ref={this.props.setRef}>
                 <input type='checkbox' href='#' className='menu-open' />
     
                 <div className='menu-open-button'>
@@ -17,18 +17,23 @@ class Nav extends Component {
                     <span className='hamburger' id='hamburger3'></span>
     
                     <span className='menu-item' id='menu-item1'>
+                        <img className='menu-item-icon' src='./images/home-icon.svg' alt='home icon link' />
                         <span className='menu-item-description'>Home</span>
                     </span>
                     <span className='menu-item' id='menu-item2'>
+                        <img className='menu-item-icon' src='./images/code-icon.svg' alt='portfolio icon link' />
                         <span className='menu-item-description'>Portfolio</span>
                     </span>
                     <span className='menu-item' id='menu-item3'>
+                        <img className='menu-item-icon' src='./images/flask.svg' alt='experiments icon link' />
                         <span className='menu-item-description'>Experiments</span>
                     </span>
                     <span className='menu-item' id='menu-item4'>
+                        <img className='menu-item-icon' src='./images/user.svg' alt='about me icon link' />
                         <span className='menu-item-description'>About</span>
                     </span>
                     <span className='menu-item' id='menu-item5'>
+                        <img className='menu-item-icon' src='./images/chat-icon.svg' alt='contact icon link' />
                         <span className='menu-item-description'>Contact</span>
                     </span>
                 </div>
