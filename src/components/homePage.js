3a43d48ce98ea415a2ebfe730c97707493e82c14
Zach Niemann <czniemann1990@gmail.com>
2018-10-07 01:52:37 -0500

    homepage testing new logo and started new vertical animation

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,56 +3,51 @@ import DistortText from './distortText';
 import ParticleLayer from './particleLayer';
 import './style/homepage.css';
 
-function DisplayLogo(props) {
-    const svg = {
-        fill: '#000000',
-        stroke: '#000000',
-        strokeWidth: 0.2,
-        strokeOpacity: 0.5,
-
-        lPath: {
-            strokeOpacity: 0.45,
-            strokeWidth: 0.3
-        },
-
-        sPath: {
-            strokeOpacity: 1,
-            strokeWidth: 0.1
-        },
-
-        node: {
-            strokeWidth: 0.2
-        }
-    };
-
-    //<path id='mid-main' className='grid-design' d='M50 50 L20 80' style={{strokeOpacity: svg.lPath.strokeOpacity, strokeWidth: svg.lPath.strokeWidth}} />
-
+function DisplayScrollDirection(props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" height='100%' width='100%' viewBox="0 0 100 100"
-            style={{fill: svg.fill, stroke: svg.stroke, strokeWidth: svg.strokeWidth, strokeOpacity: svg.strokeOpacity}}>
+        <svg xmlns="http://www.w3.org/2000/svg" height='100%' width='100%' viewBox="0 0 100 100">
+            <path id='scroll-path'  d='M0 0 h100 v100 h-100 z' />
+        </svg>
+    );
+}
 
-            <path id='top-main' className='grid-design'  d='M10 10 L90 10' style={{strokeOpacity: svg.lPath.strokeOpacity, strokeWidth: svg.lPath.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='8.5' cy='10' style={{ strokeWidth: svg.node.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='91.5' cy='10' style={{strokeWidth: svg.node.strokeWidth}} />                 
+function DisplayLogo(props) {    
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height='100%' width='100%' viewBox="0 0 100 100">
 
-            <path id='top-secondary' className='grid-design'  d='M20 13 L70 13' style={{strokeOpacity: svg.sPath.strokeOpacity, strokeWidth: svg.sPath.strokeWidth}} />
+            <path id='top-main' className='grid-main'  d='M10 10 L90 10' />
+            <circle className='grid-node' r='0.8' cx='8.5' cy='10' />
+            <circle className='grid-node' r='0.8' cx='91.5' cy='10' />                 
 
-            <path id='mid-main' className='grid-design' d='M80 20 L60 40'  style={{strokeOpacity: svg.lPath.strokeOpacity, strokeWidth: svg.lPath.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='81.5' cy='18.5' style={{strokeWidth: svg.node.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='58.5' cy='41.5' style={{strokeWidth: svg.node.strokeWidth}} />
+            <path id='top-secondary' className='grid-secondary'  d='M20 12 L70 12' />
+            <circle className='grid-node-secondary' r='0.5' cx='19.2' cy='12' />
+            <circle className='grid-node-secondary' r='0.5' cx='70.8' cy='12' />
 
-            <path id='mid-main' className='grid-design' d='M47 53 L20 80' style={{strokeOpacity: svg.lPath.strokeOpacity, strokeWidth: svg.lPath.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='48.5' cy='51.5' style={{strokeWidth: svg.node.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='18.5' cy='81.5' style={{strokeWidth: svg.node.strokeWidth}} />
 
-            <path id='mid-secondary' className='grid-design' d='M74 30 L34 70' style={{strokeOpacity: svg.sPath.strokeOpacity, strokeWidth: svg.sPath.strokeWidth}} />
+            <path id='mid-main' className='grid-main' d='M86 15 l-27 26' />
+            <circle className='grid-node' r='0.8' cx='86.8' cy='14.2' />
+            <circle className='grid-node' r='0.8' cx='58' cy='42' />
 
-            <path id='bottom-main' className='grid-design' d='M10 90 L90 90' style={{strokeOpacity: svg.lPath.strokeOpacity, strokeWidth: svg.lPath.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='8.5' cy='90' style={{strokeWidth: svg.node.strokeWidth}} />
-            <circle className='grid-node' r='0.8' cx='91.5' cy='90' style={{strokeWidth: svg.node.strokeWidth}} />
+            <path id='mid-main' className='grid-main' d='M14.8 83 l31 -30' />
+            <circle className='grid-node' r='0.8' cx='46.6' cy='52.2' />
+            <circle className='grid-node' r='0.8' cx='14' cy='84' />
 
-            <path id='bottom-secondary' className='grid-design' d='M25 87 h15' style={{strokeOpacity: svg.sPath.strokeOpacity, strokeWidth: svg.sPath.strokeWidth}} />
-            <path id='bottom-secondary' className='grid-design' d='M52 87 L86 87' style={{strokeOpacity: svg.sPath.strokeOpacity, strokeWidth: svg.sPath.strokeWidth}} />
+            <path id='mid-secondary' className='grid-secondary' d='M74 29 L32 69' />
+            <circle className='grid-node-secondary' r='0.5' cx='74.6' cy='28.4' />
+            <circle className='grid-node-secondary' r='0.5' cx='31.6' cy='69.6' />
+
+
+            <path id='bottom-main' className='grid-main' d='M10 90 L90 90' />
+            <circle className='grid-node' r='0.8' cx='8.5' cy='90' />
+            <circle className='grid-node' r='0.8' cx='91.5' cy='90' />
+
+            <path id='bottom-secondary' className='grid-secondary' d='M20 88 h15' />
+            <circle className='grid-node-secondary' r='0.5' cx='19.2' cy='88' />
+            <circle className='grid-node-secondary' r='0.5' cx='35.8' cy='88' />
+
+            <path id='bottom-secondary' className='grid-secondary' d='M52 88 h32' />
+            <circle className='grid-node-secondary' r='0.5' cx='51.2' cy='88' />
+            <circle className='grid-node-secondary' r='0.5' cx='84.8' cy='88' />
         </svg>
     );
 }
@@ -87,11 +82,12 @@ class HomePage extends Component {
                 </div>
                 
                 <div className='vertical-pointer-container'>
-                    <span className='vertical-pointer'></span>
+                    <DisplayScrollDirection />
                 </div>
             </div>           
         );
     }
 }
 
+//<span className='vertical-pointer'></span>
 export default HomePage;
