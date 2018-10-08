501dbd5e1fc7c4b6d49c189e8ac932b8239ae022
Zach Niemann <czniemann1990@gmail.com>
2018-10-08 15:56:56 -0500

    updated about me design: content is now held in a container that is the full height/width of the section until > 800px, then it is max-content

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,20 +7,22 @@ class AboutMe extends Component {
         return (
             <article id='about-me-section' ref={this.props.setRef}>
 
-                <h1 id='about-me-header' className='section-header'>About me</h1>
-                <p className='section-description' id='about-init-description'>
-                    Hi! I'm Zach, a versatile Web Developer with experience in both the front-end and back-end of the development process.
-                </p>
+                <div className='about-me-container'>
+                    <h1 id='about-me-header' className='section-header'>About me</h1>
+                    <p className='section-description' id='about-init-description'>
+                        Hi! I'm Zach, a versatile Web Developer with experience in both the front-end and back-end of the development process.
+                    </p>
 
-                <p className='section-description'>
-                    I'm always looking to grow my skill set, specializing in React and mobile-first responsive design.
-                </p>
+                    <p className='section-description'>
+                        I'm always looking to grow my skill set, specializing in React and mobile-first responsive design.
+                    </p>
 
-                <ProgressBar />
+                    <ProgressBar />
 
-                <a id='resume-link' href='./resume - Zach Niemann.pdf' download>
-                    <button className='btn slide-in mid-expand'>resume</button>
-                </a>
+                    <a id='resume-link' href='./resume - Zach Niemann.pdf' download>
+                        <button className='btn slide-in mid-expand'>resume</button>
+                    </a>
+                </div>
             </article>
         );
     }
